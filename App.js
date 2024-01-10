f97b9e6683d8741164cc149379f474c76c9ab9db
unknown <fakhrifirjatullah7@gmail.com>
2024-01-10 12:06:16 +0700

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>541211114-Fakhri</Text>
       <StatusBar style="auto" />
     </View>
   );
